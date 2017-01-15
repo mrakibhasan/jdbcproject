06f032123a3150c69391fb90567922b6a795439b
rakibthefigter <rakibcse05@gmail.com>
2017-01-14 21:54:50 -0500

    Add Description of productBO file

--- src/com/jdbc/BO/ProductBO.java
+++ src/com/jdbc/BO/ProductBO.java
@@ -8,27 +8,32 @@ import com.jdbc.DTO.Product;
 public class ProductBO {
 	ProductDaoImpl pdimp=new ProductDaoImpl();
 	
+	// fetch the product by category
 	public List<Product> getProductByCategory(int id) throws Exception{
 		return pdimp.getAllProductByCategoryId( id);
 		
 	}
-	
+	// delete product info
 	public int deleteProductInfo(int id) throws Exception{
 		return pdimp.deleteProduct(id);
 		
 	}
+	// fetch the product with largest production
 	public List<Product> getProductByLargestDescription() throws Exception{
 		return pdimp.getLargestDescription();
 		
 	}
+	//delete product from a category
 	public int deleteProductFromaCategory(int id) throws Exception{
 		return pdimp.RemoveProductfromaCategory(id);
 		
 	}
+	// fetch most recent five products
 	public List<Product> getRecentProducts() throws Exception{
 		return pdimp.DisplayRecent5Products();
 		
 	}
+	// get average number of products
 	public double getAvgNumberofproducts() throws Exception{
 		return pdimp.getAvgNumberPorductsofAllCategory();
 	}
